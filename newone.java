2653422f60116433b52ae1e75da2b487d92a8f28
watermelon158 <MiguelNatan286@gmail.com>
2024-08-13 17:29:43 +0800

    Create newone.java

---
+++ newone.java
@@ -0,0 +1,45 @@
+public class MathUtils {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+}
+public class Dog {
+    String breed;
+    int size;
+    String colour;
+    int age;
+ 
+    void eat() {
+    }
+ 
+    void run() {
+    }
+ 
+    void sleep(){
+    }
+ 
+    void name(){
+    }
+}
+public class Puppy{
+    public Puppy(){
+    }
+ 
+    public Puppy(String name){
+        // 这个构造器仅有一个参数：name
+    }
+}
+public class Puppy{
+   public Puppy(String name){
+      //这个构造器仅有一个参数：name
+      System.out.println("小狗的名字是 : " + name ); 
+   }
+   public static void main(String[] args){
+      // 下面的语句将创建一个Puppy对象
+      Puppy myPuppy = new Puppy( "tommy" );
+   }
+}
